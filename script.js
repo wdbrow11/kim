ccd836cb4523b0216c77d754c80ef8ad28a8239b
Will Brown <williambrown@williams-air.lan>
2019-07-17 23:56:41 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var shoutouts = [
+  "You always find the coolest features on your websites and you're really nice.",
+  "Your determination when playing games (it's surreal)",
+  "it's been super fun talking to you and you are super hardworking",
+  "How nice you are to everyone and how eager you are to help.",
+  "Your awesome poker face",
+  "We share common interests and I'm really glad I met you! You're so sweet and such a lovable person.",
+  "that we can talk about k-pop and skincare together! I also appreciate you for introducing me to skincharisma, I totally forgot about that site and I'm going to start using it more from now on :) I also love that I can talk about big issues with you such as the anti-vaxxers!",
+  "that you have a lot of control. I'm usually all over the place, and I hope to one day have your ability to control my thoughts as well as you.",
+  "How good you are at our group games!",
+  "How kind and smart you are.",
+  "How you contribute to conversations!",
+  "how nice and kind you are. I also love watching your projects because you always come up with some cool ideas and execute them so well",
+  "Your determination! You are so hardworking and focused and I admire that so much."
+]
+
+for (x of shoutouts) {
+  $("#shoutout-container").append("<p>" + x + "</p> <br>");
+}
